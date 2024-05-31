08d8f6149008d4ca358d213de947cd6e5f719891
Jaroslav Kysela <perex@perex.cz>
2024-05-31 11:33:24 +0200

    setup.py: licence -> license
    Signed-off-by: Jaroslav Kysela <perex@perex.cz>

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ setup(
   description="Python binding for the ALSA library.",
   url="http://www.alsa-project.org",
   download_url="ftp://ftp.alsa-project.org/pub/pyalsa/",
-  licence="LGPLv2+",
+  license="LGPLv2+",
   author="The ALSA Team",
   author_email='alsa-devel@alsa-project.org',
   ext_modules=[
